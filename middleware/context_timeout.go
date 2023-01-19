4b0939e20a22a8d9afa3156cbc31b0b210228173
hakankutluay <hakan.kutluay@yemeksepeti.com>
2023-01-19 09:32:13 +0300

    update comments

--- middleware/context_timeout.go
+++ middleware/context_timeout.go
@@ -9,30 +9,18 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-// Create handler that checks for context deadline and runs actual task in separate coroutine
-// Note: separate coroutine may not be even if you do not want to process continue executing and
-// just want to stop long-running handler to stop and you are using "context aware" methods (ala db queries with ctx)
-// 	e.GET("/", func(c echo.Context) error {
+// Simply return or wrap the error that context passed method returns.
+// Note: Works with only context timeout. Does not works with kong running processes without context.
 //
-//		doneCh := make(chan error)
-//		go func(ctx context.Context) {
-//			doneCh <- myPossiblyLongRunningBackgroundTaskWithCtx(ctx)
-//		}(c.Request().Context())
+// e.GET("/", func(c echo.Context) error {
+// 	sleepTime := time.Duration(2 * time.Second)
 //
-//		select { // wait for task to finish or context to timeout/cancelled
-//		case err := <-doneCh:
-//			if err != nil {
-//				return err
-//			}
-//			return c.String(http.StatusOK, "OK")
-//		case <-c.Request().Context().Done():
-//			if c.Request().Context().Err() == context.DeadlineExceeded {
-//				return c.String(http.StatusServiceUnavailable, "timeout")
-//			}
-//			return c.Request().Context().Err()
-//		}
+// 	if err := sleepWithContext(c.Request().Context(), sleepTime); err != nil {
+// 		return fmt.Errorf("%w: execution error", err)
+// 	}
 //
-//	})
+// 	return c.String(http.StatusOK, "Hello, World!")
+// })
 //
 
 // ContextTimeoutConfig defines the config for ContextTimeout middleware.
@@ -45,9 +33,6 @@ type ContextTimeoutConfig struct {
 	ErrorMessage string
 
 	// Timeout configures a timeout for the middleware, defaults to 0 for no timeout
-	// NOTE: when difference between timeout duration and handler execution time is almost the same (in range of 100microseconds)
-	// the result of timeout does not seem to be reliable - could respond timeout, could respond handler output
-	// difference over 500microseconds (0.5millisecond) response seems to be reliable
 	Timeout time.Duration
 }
 
@@ -60,8 +45,8 @@ var (
 	}
 )
 
-// ContextTimeout returns a middleware which returns error (503 Service Unavailable error) to client immediately when handler
-// call runs for longer than its time limit. NB: timeout does not stop handler execution.
+// ContextTimeout returns a middleware which returns error (503 Service Unavailable error) to client
+// when underlying method returns context.DeadlineExceeded error.
 func ContextTimeout() echo.MiddlewareFunc {
 	return ContextTimeoutWithConfig(DefaultContextTimeoutConfig)
 }
@@ -71,7 +56,7 @@ func ContextTimeoutWithConfig(config ContextTimeoutConfig) echo.MiddlewareFunc {
 	return config.ToMiddleware()
 }
 
-// ToMiddleware converts Config to middleware or returns an error for invalid configuration
+// ToMiddleware converts Config to middleware.
 func (config ContextTimeoutConfig) ToMiddleware() echo.MiddlewareFunc {
 	if config.Skipper == nil {
 		config.Skipper = DefaultTimeoutConfig.Skipper
